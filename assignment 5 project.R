da40524b7ced1ddd9a92b68a17c6059785e0f259
Priya Bhatnagar <pgb221@lehigh.edu>
2019-11-14 18:38:41 -0500

    edited code for chapter 5

--- assignment 5 project.R
+++ assignment 5 project.R
@@ -44,7 +44,7 @@ ptm <- proc.time()
 header <- read.table("CPIAUCSL.csv", header = TRUE,
                      sep=",", nrow = 1)
 DF <- fread("CPIAUCSL.csv", skip=1, sep=",",
-                  header=FALSE, data.table=FALSE)
+            header=FALSE, data.table=FALSE)
 setnames(DF, colnames(header))
 rm(header)
 FREAD_READ_TIME <- (proc.time() - ptm)
